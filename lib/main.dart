8a4c745cdd007f3016d90d332a67f08f131e2559
aqeelahlam <aqeelahlam@gmail.com>
2022-01-17 11:34:57 +0530

    Floating Action Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -23,26 +23,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("hello first"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Card(
-              color: Colors.blue,
-              child: Container(
-                child: const Text("Chart"),
-                // width: double.infinity,
-              ),
-              elevation: 5,
-            ),
-            UserTransactions(),
-          ],
+        appBar: AppBar(
+          title: const Text("hello first"),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Card(
+                color: Colors.blue,
+                child: Container(
+                  child: const Text("Chart"),
+                  // width: double.infinity,
+                ),
+                elevation: 5,
+              ),
+              UserTransactions(),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ));
   }
 }
